738d7a0180685f569d8cd80081e7b73a2434755e
lukakrilic96 <lukakrilic96@gmail.com>
2018-02-14 18:31:47 +0100

    poboljšao implementaciju

--- duljine/duljine/Form1.cs
+++ duljine/duljine/Form1.cs
@@ -31,64 +31,146 @@ namespace duljine
 
 
         
-        public double OmjerJedinice()
-
+       
+        public double OmjerJedinice(string veličina_1,string veličina_2)
         {
-           
+            double broj=0;
+            switch (veličina_2)
+            {
+                case "m":
+                    {
+                        if (veličina_1 == "km")
+                        {
+                            broj+= 0.001;
+                        }
 
+                        if (veličina_1 == "cm")
+                        {
+                            broj += 100;
+                        }
+
+                        if (veličina_1 == "mm")
+                        {
+                            broj += 1000;
+
+                        }
+                        if (veličina_1 == "feet")
+                        {
+                            broj += 3.28084;
+                        }
+                        if (veličina_1 == "inch")
+                        {
+                            broj += 39.3701;
+                        }
+                        if (veličina_1 == "yard")
+                        {
+                            broj += 1.09361;
+                        }
+                        break;
+                    }
+
+                case "km":
+                    {
+                        if (veličina_1 == "m")
+                        {
+                            broj += 1000;
+                        }
+
+                        if (veličina_1 == "cm")
+                        {
+                            broj += 100000;
+                        }
+                        if (veličina_1 == "mm")
+                        {
+                            broj += 1000000;
+
+                        }
+                        if (veličina_1 == "feet")
+                        {
+                            broj += 3280.84;
+                        }
+
+                        if (veličina_1 == "inch")
+                        {
+                            broj += 39370.1;
+                        }
+
+                        if (veličina_1 == "yard")
+                        {
+                            broj += 1093.61;
+                        }
+                        break;
+                    }
+                case "cm":
+                    {
+                        if (veličina_1 == "m")
+                        {
+                            broj +=  0.1;
+
+                        }
+
+                        if (veličina_1 == "km")
+                        {
+                            broj += 0.00001;
+                        }
+                        if (veličina_1== "mm")
+                        {
+                            broj += 10;
+                        }
+                        if (veličina_1 == "feet")
+                        {
+                            broj += 0.0328084;
+                        }
+
+                        if (veličina_1 == "inch")
+                        {
+                            broj += 0.393701;
+                        }
+
+                        if (veličina_1 == "yard")
+                        {
+                            broj += 0.0109361;
+                        }
+                        break;
+                    }
+                case "mm":
+                    {
+                        if (veličina_1 == "m")
+                        {
+                            broj += 0.001;
+
+                        }
+
+                        if (veličina_1 == "km")
+                        {
+                            broj += 0.000001;
+                        }
+                        if (veličina_1 == "cm")
+                        {
+                            broj += 0.1;
+
+                        }
+                        if (veličina_1 == "feet")
+                        {
+                            broj += 0.00328084;
+                        }
+
+                        if (veličina_1 == "inch")
+                        {
+                            broj += 0.0393701;
+                        }
+
+                        if (veličina_1 == "yard")
+                        {
+                            broj += 0.00109361;
+                        }
+                        break;
+                    }
+                
+                   
+            }
+            return broj;
             
-            if(ListaDuljina.SelectedIndex==0 && ListaDuljine2.SelectedIndex == 1)
-            {
-                return 0.001;
-            }
-             else if(ListaDuljina.SelectedIndex==0 && ListaDuljine2.SelectedIndex == 2)
-            {
-                return 100;
-            }
-            else if(ListaDuljina.SelectedIndex == 0 && ListaDuljine2.SelectedIndex == 3)
-            {
-                return 1000;
-            }
-            else if(ListaDuljina.SelectedIndex == 1 && ListaDuljine2.SelectedIndex == 0)
-            {
-                return 1000;
-            }
-            else if(ListaDuljina.SelectedIndex == 1 && ListaDuljine2.SelectedIndex == 2)
-            {
-                return 100000;
-            }
-            else if(ListaDuljina.SelectedIndex == 1 && ListaDuljine2.SelectedIndex == 3)
-            {
-               return 1000000;
-            }
-            else if(ListaDuljina.SelectedIndex == 2 && ListaDuljine2.SelectedIndex == 0)
-            {
-                return 0.01;
-            }
-            else if(ListaDuljina.SelectedIndex == 2 && ListaDuljine2.SelectedIndex == 1)
-            {
-                return 0.00001;
-            }
-            else if(ListaDuljina.SelectedIndex == 2 && ListaDuljine2.SelectedIndex == 3)
-            {
-                return 10;
-            }
-            else if(ListaDuljina.SelectedIndex == 3 && ListaDuljine2.SelectedIndex == 0)
-            {
-                return 0.001;
-            }
-            else if(ListaDuljina.SelectedIndex == 3 && ListaDuljine2.SelectedIndex == 1)
-            {
-                return 0.000001;
-            }
-            else if(ListaDuljina.SelectedIndex == 3 && ListaDuljine2.SelectedIndex == 2)
-            {
-                return 0.1;
-            }
-            else
-            {
-                return 1.0;
-            }
         }
 
             private void button1_Click(object sender, EventArgs e)
@@ -102,11 +184,12 @@ namespace duljine
            
             if (double.TryParse(IznosDuljine.Text, out prva_duljina) && double.TryParse(IznosDuljine2.Text, out druga_duljina))
             {
-            
+                veličina_1 = ListaDuljina.Text;
+                veličina_2 = ListaDuljine2.Text;
 
-              
-               
-                BrojVeličine = OmjerJedinice();
+
+
+                BrojVeličine = OmjerJedinice(veličina_1, veličina_2);
                  
                 
 
@@ -117,7 +200,7 @@ namespace duljine
                     if (ListaOperacije.SelectedIndex == 0)
                     {
                     
-                        prva_duljina*= BrojVeličine;
+                        druga_duljina*= BrojVeličine;
                         rezultat = prva_duljina + druga_duljina;
                         IzracunataVrijednost.Text = rezultat.ToString();
                         veličina_1 = ListaDuljina.Text;
@@ -125,8 +208,18 @@ namespace duljine
                     }
                     else
                     {
+                       
                         druga_duljina *= BrojVeličine;
-                        rezultat = prva_duljina - druga_duljina;
+
+                    if (prva_duljina < druga_duljina)
+                    {
+                        rezultat = druga_duljina - prva_duljina;
+                    }
+                    else
+                    {
+                       rezultat = prva_duljina - druga_duljina;
+                    }
+                       
                         IzracunataVrijednost.Text = rezultat.ToString();
                         veličina_1 = ListaDuljina.Text;
                     }
